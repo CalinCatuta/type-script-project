c58e3a0c5487b6b2102cb3e363f91e71bb25f4f4
CalinCatuta <calinut123@yahoo.co.uk>
2023-12-13 14:22:38 +0200

    Semafor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
+type LightState = "stop" | "slow" | "go";
+const STOP_DELAY = 3000;
+const SLOW_DELAY = 2000;
+const GO_DELAY = 5000;
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState<LightState>("go");
 
+  function getStoplightClass(light: LightState) {
+    return `light ${light}` + (state === light ? " on" : "");
+  }
+  useEffect(() => {
+    if (state === "stop") {
+      setTimeout(() => setState("go"), STOP_DELAY);
+    } else if (state === "slow") {
+      setTimeout(() => setState("stop"), SLOW_DELAY);
+    } else {
+      setTimeout(() => setState("slow"), GO_DELAY);
+    }
+  }, [state]);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="semafor">
+      <div className={getStoplightClass("stop")}></div>
+      <div className={getStoplightClass("slow")}></div>
+      <div className={getStoplightClass("go")}></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
